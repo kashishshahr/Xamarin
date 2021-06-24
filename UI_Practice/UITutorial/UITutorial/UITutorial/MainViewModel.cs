89dcee249bafdf3a6276bfb0be83c579facfce6d
kashishshahr <kashishshahr@gmail.com>
2021-06-24 21:56:37 +0530

    UI Tutorial Done

---
+++ UI_Practice/UITutorial/UITutorial/UITutorial/MainViewModel.cs
@@ -0,0 +1,51 @@
+﻿
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+
+namespace UITutorial
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        ObservableCollection<Places> _places;
+        public ObservableCollection<Places> Places { get { return _places; }
+            set {
+                _places = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Places"));
+            } }
+        public MainViewModel()
+        {
+            Places = new ObservableCollection<Places>();
+            addData();
+        }
+        private void addData()
+        {
+            Places.Add(
+                new Places
+                {
+                    Id = 0,
+                    Country = "Norway",
+                    Title = "Nature's Place",
+                    ImageSource= "https://images.pexels.com/photos/1940038/pexels-photo-1940038.jpeg"
+
+                });
+            Places.Add(
+                new Places
+                {
+                    Id = 1,
+                    Country = "Nepal",
+                    Title = "Adventure of Mountain",
+                    ImageSource= "https://images.pexels.com/photos/1531660/pexels-photo-1531660.jpeg"
+                });
+            Places.Add(
+                new Places
+                {
+                    Id = 1,
+                    Country = "Maldives",
+                    Title = "Beach Is Life Where you can Drown",
+                    ImageSource= "https://wallpapercave.com/wp/wp3398923.jpg"
+                });
+        }
+    }
+}
---
+++ UI_Practice/UITutorial/UITutorial/UITutorial/PlacesModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UITutorial
+{
+    public class Places
+    {
+     
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Country { get; set; }
+        public string ImageSource { get; set; }
+    }
+}
