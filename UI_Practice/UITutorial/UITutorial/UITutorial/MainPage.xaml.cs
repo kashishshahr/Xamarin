28b8ed9dc648f927fbdf3d0bbee571af19cbfab2
kashishshahr <kashishshahr@gmail.com>
2021-06-24 23:21:20 +0530

    navigate and scroll

--- UI_Practice/UITutorial/UITutorial/UITutorial/MainPage.xaml.cs
+++ UI_Practice/UITutorial/UITutorial/UITutorial/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace UITutorial
 {
@@ -13,6 +14,23 @@ namespace UITutorial
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        
+        private int tapCount=0;
+
+        private async void OnTapGestureRecognizerTapped(object sender, EventArgs e)
+        {
+            tapCount++;
+            
+            if (tapCount % 2 == 0)
+            {
+                await Navigation.PushAsync(new DetailPage());
+            }
+            else
+            {
+                Console.WriteLine("TAPPPPP");
+            }
         }
     }
 }
