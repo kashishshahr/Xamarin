0923b12c6dea4679fd8235ca62bc7faec02cc71c
kashishshahr <kashishshahr@gmail.com>
2021-06-26 00:08:25 +0530

    UI_Login Page

---
+++ UI_Practice/UIPractice2/UIPractice2.Android/CustomRenderer/MyEntryRenderer.cs
@@ -0,0 +1,34 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UIPractice2.CustomRenderer;
+using UIPractice2.Droid.CustomRenderer;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(MyCustomEntry),typeof(MyEntryRenderer))]
+namespace UIPractice2.Droid.CustomRenderer
+{
+    public class MyEntryRenderer:EntryRenderer
+    {
+        public MyEntryRenderer(Context context):base(context)
+        {
+
+        }
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            if(Control!=null)
+            {
+                Control.SetBackgroundColor(Android.Graphics.Color.Transparent);
+            }
+        }
+    }
+}
---
+++ UI_Practice/UIPractice2/UIPractice2/CustomRenderer/MyCustomEntry.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace UIPractice2.CustomRenderer
+{
+    public class MyCustomEntry :Entry
+    {
+
+    }
+}
