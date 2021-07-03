a170e2eb473ef447f3a6a600748e23dbef85aec3
kashishshahr <kashishshahr@gmail.com>
2021-07-04 00:01:38 +0530

    Time zone app

--- MSDN_Practice/UI_Msdn_Learn/mslearn-xamarin/TipCalc/TipCalc/TipCalc/MainPage.xaml.cs
+++ MSDN_Practice/UI_Msdn_Learn/mslearn-xamarin/TipCalc/TipCalc/TipCalc/MainPage.xaml.cs
@@ -13,9 +13,11 @@ namespace TipCalc
         public MainPage()
         {
             InitializeComponent();
+
             billInput.TextChanged += (s, e) => CalculateTip(false, false);
-            roundDown.Clicked+= (s, e) => CalculateTip(false, true);
+            roundDown.Clicked += (s, e) => CalculateTip(false, true);
             roundUp.Clicked += (s, e) => CalculateTip(true, false);
+
             tipPercentSlider.ValueChanged += (s, e) =>
             {
                 double pct = Math.Round(e.NewValue);
@@ -23,35 +25,34 @@ namespace TipCalc
                 CalculateTip(false, false);
             };
         }
+
         void CalculateTip(bool roundUp, bool roundDown)
         {
             double t;
-            if(Double.TryParse(billInput.Text,out t)&&t>0)
+            if (Double.TryParse(billInput.Text, out t) && t > 0)
             {
                 double pct = Math.Round(tipPercentSlider.Value);
-                double tip = Math.Round(t*(pct/100.0),2);
+                double tip = Math.Round(t * (pct / 100.0), 2);
+
                 double final = t + tip;
-                if(roundUp)
+
+                if (roundUp)
                 {
                     final = Math.Ceiling(final);
                     tip = final - t;
-                }else if(roundDown)
+                }
+                else if (roundDown)
                 {
                     final = Math.Floor(final);
                     tip = final - t;
                 }
+
                 tipOutput.Text = tip.ToString("C");
                 totalOutput.Text = final.ToString("C");
             }
         }
-            private void OnNormalTip(object sender, EventArgs e)
-        {
-            tipPercentSlider.Value = 15;
-        }
 
-        private void OnGenerousTip(object sender, EventArgs e)
-        {
-            tipPercentSlider.Value = 20;
-        }
+        void OnNormalTip(object sender, EventArgs e) { tipPercentSlider.Value = 15; }
+        void OnGenerousTip(object sender, EventArgs e) { tipPercentSlider.Value = 20; }
     }
 }
---
+++ MSDN_Practice/WatchHere/WatchHere/WatchHere/MainPage.xaml.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WatchHere
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Color AmColor = Color.White;
+            Color PmColor = Color.Cyan;
+            //List<String> times = new List<string>()
+            //{
+            //    "Indian/Antananarivo","Indian/Chagos","Indian/Christmas","Indian/Cocos","Indian/Comoro","Indian/Kerguelen","Indian/Mahe","Indian/Maldives","Indian/Mauritius","Indian/Mayotte","Indian/Reunion"
+            //};
+            //Console.WriteLine("Kashish");
+            //// Get All Time Zones  
+            //foreach (TimeZoneInfo z in TimeZoneInfo.GetSystemTimeZones())
+            //{
+            //    TimeZoneInfo estZone = TimeZoneInfo.FindSystemTimeZoneById(z.Id.ToString());
+            //    DateTime canTime = TimeZoneInfo.ConvertTime(DateTime.Now, TimeZoneInfo.Local, estZone);
+            //    Console.WriteLine("Time= {0}:{1}", z.Id, canTime);
+            //}
+
+
+            Device.StartTimer(TimeSpan.FromSeconds(1), () =>
+            {
+                TimeZoneInfo indianZone = TimeZoneInfo.FindSystemTimeZoneById("Asia/Kolkata");
+                TimeZoneInfo CanadaTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Canada/Eastern");
+                TimeZoneInfo GermanyTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Europe/Berlin");
+                TimeZoneInfo LondonTimeZOne = TimeZoneInfo.FindSystemTimeZoneById("Europe/London");
+
+                DateTime indianZoneTime = TimeZoneInfo.ConvertTime(DateTime.Now, TimeZoneInfo.Local, indianZone);
+
+                if (indianZoneTime.ToString("tt") == "AM")
+                {
+                    IndianNameLabel.TextColor =  AmColor;
+                    IndianTimeLabel.TextColor =  AmColor;
+
+                }
+                else
+                {
+                    IndianTimeLabel.TextColor =  PmColor;
+                    IndianNameLabel.TextColor =  PmColor;
+                }
+
+                IndianNameLabel.Text = "India";
+                IndianTimeLabel.Text = indianZoneTime.ToString("dd/MM/yyyy    hh:mm:ss:ttt");
+                //------------------------------------------------------------------
+                DateTime CanadaTime = TimeZoneInfo.ConvertTime(DateTime.Now, TimeZoneInfo.Local, CanadaTimeZone);
+                TorrontoNameLabel.Text = "Canada:Torronto";
+                TorrontoTimeLabel.Text = CanadaTime.ToString("dd/MM/yyyy    hh:mm:ss:ttt");
+                if (CanadaTime.ToString("tt") == "AM")
+                {
+                    TorrontoNameLabel.TextColor =  AmColor;
+                    TorrontoTimeLabel.TextColor =  AmColor;
+
+                }
+                else
+                {
+                    TorrontoNameLabel.TextColor =  PmColor;
+                    TorrontoTimeLabel.TextColor =  PmColor;
+                }
+                DateTime GermanyTime = TimeZoneInfo.ConvertTime(DateTime.Now, TimeZoneInfo.Local, GermanyTimeZone);
+                FrankfurtNameLabel.Text = "Germany";
+                FrankfurtTimeLabel.Text = GermanyTime.ToString("dd/MM/yyyy    hh:mm:ss tt");
+                if (GermanyTime.ToString("tt") == "AM")
+                {
+                    FrankfurtNameLabel.TextColor =  AmColor;
+                    FrankfurtTimeLabel.TextColor =  AmColor;
+
+                }
+                else
+                {
+                    FrankfurtNameLabel.TextColor =  PmColor;
+                    FrankfurtTimeLabel.TextColor =  PmColor;
+                }
+                DateTime LondonTime = TimeZoneInfo.ConvertTime(DateTime.Now, TimeZoneInfo.Local, LondonTimeZOne);
+                LondonNameLabel.Text = "London";
+                LondonTimeLabel.Text = LondonTime.ToString("dd/MM/yyyy    hh:mm:ss:ttt");
+                if (LondonTime.ToString("tt") == "AM")
+                {
+                    LondonNameLabel.TextColor =  AmColor;
+                    LondonTimeLabel.TextColor =  AmColor;
+
+                }
+                else
+                {
+                    LondonNameLabel.TextColor =  PmColor;
+                    LondonTimeLabel.TextColor =  PmColor;
+                }
+                return true;
+            });
+        }
+
+        private void ImageTapped(object sender, EventArgs e)
+        {
+            
+        }
+    }
+
+    [ContentProperty(nameof(Source))]
+    public class ImageResourceExtension : IMarkupExtension
+    {
+        public String Source { get; set; }
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Source == null)
+                return null;
+            else
+            {
+                var imageSource = ImageSource.FromResource(Source, typeof(ImageResourceExtension).GetTypeInfo().Assembly);
+                return imageSource;
+
+            }
+        }
+    }
+}
